7e6c4f48a075310bab95f41ee6081c8b79f562c9
unknown <mal.qurany@gmail.com>
2022-02-09 07:22:26 +0700

    fix init db

--- db/imports/init.sql
+++ db/imports/init.sql
@@ -65,6 +65,7 @@ CREATE TABLE public.movies (
     runtime integer,
     rating integer,
     mpaa_rating character varying,
+    poster text,
     created_at timestamp without time zone,
     updated_at timestamp without time zone
 );
@@ -165,11 +166,11 @@ COPY public.genres (id, genre_name, created_at, updated_at) FROM stdin;
 -- Data for Name: movies; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.movies (id, title, description, year, release_date, runtime, rating, mpaa_rating, created_at, updated_at) FROM stdin;
-1	The Shawshank Redemption	Two imprisoned men bond over a number of years	1994	1994-10-14	142	5	R	2021-05-17 00:00:00	2021-05-17 00:00:00
-2	The Godfather	The aging patriarch of an organized crime dynasty transfers control to his son	1972	1972-03-24	175	5	R	2021-05-17 00:00:00	2021-05-17 00:00:00
-4	American Psycho	 A wealthy New York investment banking executive hides his alternate psychopathic ego	2000	2000-04-14	102	4	R	2021-05-17 00:00:00	2021-05-17 00:00:00
-3	The Dark Knight	The menace known as the Joker wreaks havoc on Gotham City	2008	2008-07-18	152	5	PG13	2021-05-17 00:00:00	2021-05-17 00:00:00
+COPY public.movies (id, title, description, year, release_date, runtime, rating, mpaa_rating, poster, created_at, updated_at) FROM stdin;
+1	The Shawshank Redemption	Two imprisoned men bond over a number of years	1994	1994-10-14	142	5	R	NULL	2021-05-17 00:00:00	2021-05-17 00:00:00
+2	The Godfather	The aging patriarch of an organized crime dynasty transfers control to his son	1972	1972-03-24	175	5	R	NULL	2021-05-17 00:00:00	2021-05-17 00:00:00
+4	American Psycho	 A wealthy New York investment banking executive hides his alternate psychopathic ego	2000	2000-04-14	102	4	R	null	2021-05-17 00:00:00	2021-05-17 00:00:00
+3	The Dark Knight	The menace known as the Joker wreaks havoc on Gotham City	2008	2008-07-18	152	5	PG13	NULL	2021-05-17 00:00:00	2021-05-17 00:00:00
 \.
 
 
